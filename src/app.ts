e6b3bc1e03d0c3ec3bb91f1fd4bef17583c7eb48
AnassAzeroual <azeroual.anass2015@gmail.com>
2021-05-07 11:38:44 +0000

    start express

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,9 @@
-const app = "Hi !"
+import express from "express"
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('test get /');
+});
+
+app.listen(5000, () => console.log('Server running'));

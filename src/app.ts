b70cf1bd95e2ef69a9c07fb1e95d3c91fd07f294
AnassAzeroual <azeroual.anass2015@gmail.com>
2021-05-07 12:05:01 +0000

    add notitions

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
-import express from "express"
+import express, { Application, Request, Response, NextFunction } from "express"
 
-const app = express();
+const app: Application = express();
 
-app.get('/', (req, res) => {
-    res.send('test get /');
+const add = (a: number, b: number) => a + b;
+app.get('/', (req: Request, res: Response) => {
+    console.log(add(5, 6));
+
+    res.send('test get / 2020');
 });
 
 app.listen(5000, () => console.log('Server running'));

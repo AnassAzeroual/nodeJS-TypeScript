7ef4aa13f77ba3c01e85595e11aa4ca5935d244c
AnassAzeroual <azeroual.anass2015@gmail.com>
2021-05-07 14:24:15 +0000

    config router and controller

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,40 @@
-import * as bodyParser from "body-parser";
-import express, { Application, Request, Response, NextFunction } from "express"
+import express, { Application, Request, Response, NextFunction } from "express";
+import http from 'http';
+var cors = require('cors');
+import routesModule from './routes/routerModule';
 
 const router: Application = express();
+
+const allowlist = ['localhost:5000', 'http://example2.com']
+const corsOptionsDelegate = function (req: Request, callback: (arg0: null, arg1: any) => void) {
+    let corsOptions;
+    if (allowlist.indexOf(String(req.header('Origin'))) !== -1) {
+        corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+    } else {
+        corsOptions = { origin: false } // disable CORS for this request
+    }
+    console.log(String(req.header('Origin')));
+    console.log(corsOptions);
+
+    callback(null, corsOptions) // callback expects two parameters: error and options
+}
+router.use(cors(corsOptionsDelegate))
 router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
 
 
-router.use((req: Request, res: Response, next: NextFunction) => {
-    res.header('Acc')
-})
 
-router.listen(5000, () => console.log('Server running'));
+/** Routes go here */
+router.use('/api/sample', routesModule);
+
+/** Error handling */
+router.use((req, res, next) => {
+    const error = new Error('Not found');
+
+    res.status(404).json({
+        message: error.message
+    });
+});
+
+const httpServer = http.createServer(router);
+httpServer.listen(5000, () => console.log('Server running'));
